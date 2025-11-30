066e64d4894c799bde93f829e7dcc95cf6920df5
Negro-13 <max.avt8@gmail.com>
2025-11-30 12:30:26 -0300

    arreglo de repuestos

--- mi_proyecto/Taller/db.py
+++ mi_proyecto/Taller/db.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="127.0.0.1",
         user="root",
         password="root",
         database="taller_mecanico_dj",
-        port=3306
+        port=3308
     )
